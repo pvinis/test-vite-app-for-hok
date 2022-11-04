e6f700cf3be8f832f810f2de55edcd95c10b9ba1
Pavlos Vinieratos <pvinis@gmail.com>
2022-11-04 15:12:07 +0000

    batman

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { toPairs } from "remeda";
+import { toString } from "lodash";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>wow now that's a cool app!</h1>
+      <h2>env values</h2>
+      <ul>
+        {toPairs(import.meta.env).map(([k, v]) => (
+          <li key={k}>
+            {k}: <code style={{ backgroundColor: "pink" }}>{toString(v)}</code>
+          </li>
+        ))}
+      </ul>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
